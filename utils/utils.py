9eb1ab557b57381682aee8ad579c6073f074cdbc
HD-Harsh-Doshi <harshdoshi2110@gmail.com>
2026-04-26 05:45:03 +0530

    utils

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,8 @@ def process_data(df):
     df['camp_date_dt'] = pd.to_datetime(df['camp_date'], format='%m-%d-%y', errors='coerce')
 
     # 2. Extract the Year while it is still a Datetime object
-    df['Year'] = df['camp_date_dt'].dt.year.fillna(0).astype(int)
+    # df['Year'] = df['camp_date_dt'].dt.year.fillna(0).astype(int)
+    df['Year'] = df['camp_date_dt'].dt.year.astype('Int64')
 
     # 3. NOW convert the column to a date-only format for display
     df['camp_date_dt'] = df['camp_date_dt'].dt.date
